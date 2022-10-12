9fac8f1b4b8fe297f0a64d6f28afc63acf450a55
Gwang Min <gmgpgk1713@gmail.com>
2022-10-12 16:40:38 +0900

    5장의 카드르 랜덤배분

--- app/src/main/java/kr/ac/kumoh/s20180088/com/example/w0501carddealer/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/s20180088/com/example/w0501carddealer/MainActivity.kt
@@ -2,10 +2,55 @@ package kr.ac.kumoh.s20180088.com.example.w0501carddealer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kr.ac.kumoh.s20180088.com.example.w0501carddealer.databinding.ActivityMainBinding
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnDeal.setOnClickListener {
+            //binding.card1.setImageResource(R.drawable.c_10_of_clubs)
+            val res = IntArray(5)
+            for (i in res.indices) {
+                res[i] = resources.getIdentifier(
+                    getCardName(Random.nextInt(52)),
+                    "drawable",
+                    packageName
+                )
+            } // indices -> index가 될 수 있는것 이라는 의미
+
+            binding.card1.setImageResource(res[0])
+            binding.card2.setImageResource(res[1])
+            binding.card3.setImageResource(res[2])
+            binding.card4.setImageResource(res[3])
+            binding.card5.setImageResource(res[4])
+        }
     }
+
+    private fun getCardName(c: Int): String {
+        val shape = when( c/13 ) {
+            0 -> "spades"
+            1 -> "diamonds"
+            2 -> "hearts"
+            3 -> "clubs"
+            else -> "error"
+        }
+
+        val number = when ( c%13 ) {
+            0 -> "ace"
+            in 1..9 -> ( c%13+1).toString()
+            10 -> "jack"
+            11 -> "queen"
+            12 -> "king"
+            else -> "error"
+        }
+
+        return "c_${number}_of_${shape}"
+    }
+    // TODO: 스트링 변환
 }
